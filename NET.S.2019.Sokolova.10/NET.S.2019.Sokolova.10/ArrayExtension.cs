73357a47c79f96fcfc940be52deb39b085d97cff
Marina Sokolova <sokolova.m.e9@gmail.com>
2019-04-23 12:04:16 +0300

    add README

---
+++ NET.S.2019.Sokolova.10/NET.S.2019.Sokolova.10/ArrayExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Sokolova._10
+{
+    public static class ArrayExtension
+    {
+        public static TSource[] SortBy<TSource>(this TSource[] source, IComparer<TSource> comparer)
+        {
+            List<TSource> listResult = new List<TSource>();
+            
+            foreach(var item in source)
+            {
+                listResult.Add(item);
+            }
+
+            for (int i = 0; i < listResult.Count; i++)
+            {
+                for (int j = 0; j < listResult.Count - i - 1; j++)
+                {
+                    if (comparer.Compare(listResult[j], listResult[j + 1]) > 0)
+                    {
+                        TSource temp = listResult[j];
+                        listResult[j] = listResult[j + 1];
+                        listResult[j + 1] = temp;
+                    }
+                }
+            }
+
+            return listResult.ToArray();
+        }
+
+        public static TSource[] Filter<TSource>(this TSource[] source, IPredicate<TSource> predicate)
+        {
+            List<TSource> resultList = new List<TSource>();
+
+            for(int i = 0; i < source.Length; i++)
+            {
+                if(predicate.IsPredicate(source[i]))
+                {
+                    resultList.Add(source[i]);
+                }
+            }
+
+            return resultList.ToArray();
+        }
+    }
+}
---
+++ NET.S.2019.Sokolova.10/NET.S.2019.Sokolova.10/IPredicate.cs
@@ -0,0 +1,6 @@
+﻿namespace NET.S._2019.Sokolova._10
+{
+    public interface IPredicate<TSource>
+    {
+    }
+}
